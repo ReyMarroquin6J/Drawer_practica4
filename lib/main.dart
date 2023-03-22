b1464317bef9a1d85fef4ad0209c136eb180bd5d
REY_DAVID_MARROQUIN_MORALES <a.20308051281231@cbtis128.edu.mx>
2023-03-22 19:03:14 +0000

    Drawer5

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static const String _title = 'Abarrotes Don Rey';
+  static const String _title = 'Rey el killer';
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -28,74 +28,100 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  final GlobalKey<ScaffoldState> _key = GlobalKey();
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Don Chingon'),
-      ),
-      backgroundColor: const Color(0xfffff2ff),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: const Color(0xff764abc)),
-              accountName: Text(
-                "Rey David Marroquin Morales",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              accountEmail: Text(
-                "a.20308051281231@cbtis128.edu.mx",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              currentAccountPicture: FlutterLogo(),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-              ),
-              title: const Text('Page 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.person,
-              ),
-              title: const Text('Pagina 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.airplane_ticket,
-              ),
-              title: const Text('Pagina 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.car_crash,
-              ),
-              title: const Text('Pagina 3'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+        key: _key,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Rey David El Killer'),
+          backgroundColor: const Color(0xffa215ae),
         ),
-      ),
-      body: Center(),
-    );
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                // <-- SEE HERE
+                decoration: BoxDecoration(color: const Color(0xfffb270b)),
+                accountName: Text(
+                  "Rey David Marroquin Morales",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                accountEmail: Text(
+                  "A.20308051281231@cbtis128.edu.mx",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                currentAccountPicture: FlutterLogo(),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.home,
+                ),
+                title: const Text('Pagina 1'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.train,
+                ),
+                title: const Text('Pagina 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.access_alarm,
+                ),
+                title: const Text('Pagina 3'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              AboutListTile(
+                // <-- SEE HERE
+                icon: Icon(
+                  Icons.info,
+                ),
+                child: Text('About app'),
+                applicationIcon: Icon(
+                  Icons.local_play,
+                ),
+                applicationName: 'My Cool App',
+                applicationVersion: '1.0.25',
+                applicationLegalese: '© 2019 Company',
+                aboutBoxChildren: [
+                  ///Content goes here...
+                ],
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _key.currentState!.openDrawer(); //<-- SEE HERE
+                },
+                child: const Text(
+                  'Elevated Button 1',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
